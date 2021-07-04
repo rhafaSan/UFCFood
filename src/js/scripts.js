1c9617b8cf48df9731930f43028fad985315faf6
Rafael Santos <rhafa99ss@gmail.com>
2021-07-04 11:50:43 -0300

    validacoes de cadastro feitas

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,26 @@
 function goToRegister(e){
     e.preventDefault();
-    // console.log(window.location.pathname)
     window.location.href = "../register/register.html"
 }
 
 
 function watchRegister(e){
-    e.preventDefault()
+    // e.preventDefault()
     const form = document.forms.register_form;
     form.addEventListener('submit', (e) => {
         e.preventDefault()
     })
     const { username } = form;
+    var senha = document.getElementById('password');
+    console.log(senha.value.length)
+
     if(username.value.length < 4){
         window.alert('Nome de usuário precisa ser maior que 4 digítos')
+    }else if(senha.value.length <= 5){
+        window.alert('A senha precisa ser maior ou igual a 6 dígitos')
+    }else{
+        window.location.href = "../login/index.html"
     }
-    console.log(username.value);
+
+
 }
